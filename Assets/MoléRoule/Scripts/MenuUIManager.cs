2c3c5c0282f63863c000c1702a013fae5b19efa2
Adryan <adryan.hauton@gmail.com>
2022-01-03 17:31:01 +0100

    ScriptsCreation

--- Assets/MoléRoule/Scripts/MenuUIManager.cs
+++ Assets/MoléRoule/Scripts/MenuUIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,21 @@ using UnityEngine.SceneManagement;
 public class MenuUIManager : MonoBehaviour
 {
 
+    public GameObject Menu;
+    public GameObject WorldsSelection;
+
+    private void Start()
+    {
+        Menu.SetActive(true);
+        WorldsSelection.SetActive(false);
+    }
+
+    public void WorldSelect()
+    {
+        Menu.SetActive(false);
+        WorldsSelection.SetActive(true);
+    }
+    
     public void Play()
     {
         SceneManager.LoadScene("Lvl1");
